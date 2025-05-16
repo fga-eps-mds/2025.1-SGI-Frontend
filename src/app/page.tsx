5aec5d6f64ca7ee57d4c20703f51b1b05e4b3fd7
daniboycam <danielbatista1908112@gmail.com>
2025-05-16 10:14:04 -0300

    feat: adiciona componentes retangulo topo e entrar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
 'use client'
 
-import styles from "./page.module.css";
+import styles from './page.module.css'; // importando os estilos do arquivo page.module.css
 
-export default function Home() {
-  
-  const loginUrl = `https://github.com/`
-
+export default function Home() { // definir o componente principal da página 
   return (
-    <div className={styles.container}>
-      <div className={styles.card}>
-        <button className={styles.button} onClick={() => window.location.href = loginUrl}>
-          <img src="/github_icon.png" alt="GitHub Logo" className={styles.icon} />
-          <span>Logar com GitHub</span>
-        </button>
+    <>
+      {/* Primeira seção com retângulos */}
+      <main> {/* tag semântica para o conteúdo principal */}
+        <div className={styles.retanguloTopo}></div> {/* aplicar a classe importada */}
+        <div className={styles.retanguloEntrar}>
+          Entrar
+        </div>
+      </main>
+
+      {/* Segunda seção com login do GitHub */}
+      <div className={styles.container}>
+        <div className={styles.card}>
+          <button 
+            className={styles.button} 
+            onClick={() => window.location.href = 'https://github.com/'}
+          >
+            <img src="/github_icon.png" alt="GitHub Logo" className={styles.icon} />
+            <span>Logar com GitHub</span>
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   );
 }

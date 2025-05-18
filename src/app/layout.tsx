d71c1da75c1d7c839be7f85be29d3cc4d6c2e2fc
Guilherme <tegnoue@outlook.com>
2025-05-18 19:36:02 -0300

    feat: cria componente Header e aplica a todas as paginas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
+import Header from "./Componentes/Header/page";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-br">
+      <Header/>
       <body className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable}`}>
         {children}
       </body>

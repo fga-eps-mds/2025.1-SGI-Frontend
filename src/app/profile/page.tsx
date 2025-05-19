893c504e44234074dc435c9665135d65404ed7d0
Guilherme <tegnoue@outlook.com>
2025-05-19 19:36:34 -0300

    feat: inclui modal

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,34 +1,57 @@
-'use client'
-import styles from './profile.module.css'
+'use client';
+
+import { useState } from 'react';
+import Modal from '../Componentes/Modal/page';
+import styles from './profile.module.css';
 
 export default function ProfilePage() {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const DeleteAccount = () => {
+    console.log('Conta apagada!');
+    setModalOpen(false);
+  };
+
+  const Logout = () => {
+    console.log('Você saiu.');
+  }
+
   return (
-<>
+    <>
+      <main className={styles.container}>
 
-    <main className={styles.container}>
-      {/* Foto e dados agrupados para controlar o gap */}
-      <div className={styles.userInfo}>
-        <img
-          src="/sample_profile.png"
-          alt="Foto de Perfil"
-          className={styles.profileImage}
-        />
-        <span className={styles.name}>Nome do Usuário</span>
-        <span className={styles.username}>@usuario</span>
-        <span className={styles.email}>emailusuario@usuario.com</span>
-        <p className={styles.bio}>
-          Biografia: Lorem ipsum dolor sit amet consectetur adipisicing elit...
-        </p>
-      </div>
+        <div className={styles.userInfo}>
+          <img
+            src="/sample_profile.png"
+            alt="Foto de Perfil"
+            className={styles.profileImage}
+          />
+          <span className={styles.name}>Nome do Usuário</span>
+          <span className={styles.username}>@usuario</span>
+          <span className={styles.email}>emailusuario@usuario.com</span>
+          <p className={styles.bio}>
+            Biografia: Lorem ipsum dolor sit amet consectetur adipisicing elit...
+          </p>
+        </div>
 
-      <div className={styles.buttons}>
-        <button className={styles.logout}>Logout</button>
-        <button className={styles.delete}>Apagar minha conta</button>
-      </div>
+        <div className={styles.buttons}>
+          <button className={styles.logout} onClick={Logout }>Logout</button> {/* No momento as funções confirmam pelo console, corrigir. */}
+          <button className={styles.delete} onClick={() => setModalOpen(true)}>
+            Apagar minha conta
+          </button>
+        </div>
+      </main>
 
-
-    </main>
-</>
-  )
-
+      {/* Modal q confirma Apagar conta*/}
+      <Modal
+        isOpen={modalOpen}
+        onClose={() => setModalOpen(false)}
+        onConfirm={DeleteAccount}
+        title="Apagar conta?"
+        message="Deseja realmente apagar sua conta? Essa ação não poderá ser desfeita."
+        confirmText="Apagar"
+        cancelText="Cancelar"
+      />
+    </>
+  );
 }

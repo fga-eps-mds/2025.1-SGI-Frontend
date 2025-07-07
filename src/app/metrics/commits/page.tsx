b66a5a5bb2dd816cce260e04e45a73420978c528
Guilherme <tegnoue@outlook.com>
2025-07-07 19:41:13 -0300

    Esqueleto da tela de commits

--- src/app/metrics/commits/page.tsx
+++ src/app/metrics/commits/page.tsx
@@ -6,10 +6,14 @@ import styles from './commits.module.css'
 export default function Commits(){
 
     return(
-        <main>
+        <main className={styles.container}>
             <div className={styles.Top}>
-                <p className={styles.BackButton}> Voltar</p>
-                <h1> COMMITS REALIZADAS</h1>
+                <div className={styles.backButtonDiv}>
+                     <span className={`${styles.icon} material-symbols-outlined`}>arrow_back</span> <p className={styles.BackButton}> Voltar</p>
+                </div>
+                <div className={styles.titleDiv}>
+                    <h1> COMMITS REALIZADAS</h1>
+                </div>
             </div>
             <div className={styles.Bottom}>
 

8f7e8bcf25bac01872bcc745aae2f19898eccd1f
Guilherme <tegnoue@outlook.com>
2025-07-14 19:27:27 -0300

    Insere <LayoutGroup> na layout.tsx para permitir motions entre as paginas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono, Roboto } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header/page";
 import { AuthProvider } from "../hooks/useAuth";
+import { LayoutGroup } from "framer-motion";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,7 +39,9 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} ${roboto.variable}`}>
         <AuthProvider>
           <Header />
+          <LayoutGroup>
           {children}
+          </LayoutGroup>
         </AuthProvider>
       </body>
     </html>
